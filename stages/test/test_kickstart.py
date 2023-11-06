95956e5a5d267cca957876444d3b772d7dce7a68
Michael Vogt <mvo@ubuntu.com>
2023-11-06 09:58:02 +0100

    fixup! make test cases a dataclass for reability (but maybe overkill so feel free to reject)

--- stages/test/test_kickstart.py
+++ stages/test/test_kickstart.py
@@ -2,34 +2,40 @@
 
 import os.path
 import tempfile
+from dataclasses import dataclass
 
 import pytest
 from org_osbuild_kickstart import main as kickstart_main
 
 
+@dataclass
+class KsTestCase:
+    """KsTestCase is a single test case for the kickstart stage
+
+    options:  a dict with the options passed to the kickstart stage
+    expected: the expected string in the generated kickstart file
+    """
+    options: str
+    expected: str
+
+
 @pytest.fixture(name="ks_test_cases")
 def ks_test_cases_fixtures():
     return [
-        {
-            "options": {"lang": "en_US.UTF-8"},
-            "expected": "lang en_US.UTF-8",
-        },
-        {
-            "options": {"keyboard": "us"},
-            "expected": "keyboard us",
-        },
-        {
-            "options": {"timezone": "UTC"},
-            "expected": "timezone UTC",
-        },
-        {
-            "options": {
+        KsTestCase(
+            options={"lang": "en_US.UTF-8"}, expected="lang en_US.UTF-8"),
+        KsTestCase(
+            options={"keyboard": "us"}, expected="keyboard us"),
+        KsTestCase(
+            options={"timezone": "UTC"}, expected="timezone UTC"),
+        KsTestCase(
+            options={
                 "lang": "en_US.UTF-8",
                 "keyboard": "us",
                 "timezone": "UTC",
             },
-            "expected": "lang en_US.UTF-8\nkeyboard us\ntimezone UTC",
-        },
+            expected="lang en_US.UTF-8\nkeyboard us\ntimezone UTC",
+        ),
     ]
 
 
@@ -38,9 +44,9 @@ def test_kickstart(ks_test_cases):
     with tempfile.TemporaryDirectory("kickstart-test-") as tmpdir:
         for tc in ks_test_cases:
             options = {"path": ks_path}
-            options.update(tc["options"])
+            options.update(tc.options)
             kickstart_main(tmpdir, options)
 
             with open(os.path.join(tmpdir, ks_path), encoding="utf-8") as fp:
                 ks_content = fp.read()
-            assert ks_content == tc["expected"] + "\n"
+            assert ks_content == tc.expected + "\n"
