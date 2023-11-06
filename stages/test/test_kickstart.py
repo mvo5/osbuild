815c5ed3e17579b16f6516272a3542bc6b710401
Michael Vogt <mvo@ubuntu.com>
2023-11-06 09:44:40 +0100

    fixup! rewrite in pytest/fixture style (thanks to Simon!)

--- stages/test/test_kickstart.py
+++ stages/test/test_kickstart.py
@@ -1,53 +1,46 @@
 #!/usr/bin/python3
 
 import os.path
-import shutil
-import unittest
-from tempfile import mkdtemp
+import tempfile
 
+import pytest
 from org_osbuild_kickstart import main as kickstart_main
 
-test_cases = [
-    {
-        "options": {"lang": "en_US.UTF-8"},
-        "expected": "lang en_US.UTF-8",
-    },
-    {
-        "options": {"keyboard": "us"},
-        "expected": "keyboard us",
-    },
-    {
-        "options": {"timezone": "UTC"},
-        "expected": "timezone UTC",
-    },
-    {
-        "options": {
-            "lang": "en_US.UTF-8",
-            "keyboard": "us",
-            "timezone": "UTC",
+
+@pytest.fixture(name="ks_test_cases")
+def ks_test_cases_fixtures():
+    return [
+        {
+            "options": {"lang": "en_US.UTF-8"},
+            "expected": "lang en_US.UTF-8",
         },
-        "expected": "lang en_US.UTF-8\nkeyboard us\ntimezone UTC",
-    },
-]
+        {
+            "options": {"keyboard": "us"},
+            "expected": "keyboard us",
+        },
+        {
+            "options": {"timezone": "UTC"},
+            "expected": "timezone UTC",
+        },
+        {
+            "options": {
+                "lang": "en_US.UTF-8",
+                "keyboard": "us",
+                "timezone": "UTC",
+            },
+            "expected": "lang en_US.UTF-8\nkeyboard us\ntimezone UTC",
+        },
+    ]
 
 
-class TestOrgOsbuilderKickstart(unittest.TestCase):
-
-    def setUp(self):
-        self.tmpdir = mkdtemp()
-        self.addCleanup(shutil.rmtree, self.tmpdir)
-
-    def test_kickstart(self):
-        ks_path = "kickstart/kfs.cfg"
-        for tc in test_cases:
+def test_kickstart(ks_test_cases):
+    ks_path = "kickstart/kfs.cfg"
+    with tempfile.TemporaryDirectory("kickstart-test-") as tmpdir:
+        for tc in ks_test_cases:
             options = {"path": ks_path}
             options.update(tc["options"])
-            kickstart_main(self.tmpdir, options)
+            kickstart_main(tmpdir, options)
 
-            with open(os.path.join(self.tmpdir, ks_path), encoding="utf-8") as fp:
+            with open(os.path.join(tmpdir, ks_path), encoding="utf-8") as fp:
                 ks_content = fp.read()
-            self.assertEqual(ks_content, tc["expected"] + "\n")
-
-
-if __name__ == "__main__":
-    unittest.main()
+            assert ks_content == tc["expected"] + "\n"
