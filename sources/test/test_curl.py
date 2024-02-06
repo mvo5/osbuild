13cefb79b9551e948475626a667e7e34244ddc53
Michael Vogt <michael.vogt@gmail.com>
2024-02-06 17:25:54 +0100

    curl: simplify downoad data structures
    Instead of always passing a "checksum" and a "download description"
    dict unify them into a single `desc` dict that also contains the
    checksum. That makes passing the data around, especially when
    partitioning a lot simpler and easier to read.

--- sources/test/test_curl.py
+++ sources/test/test_curl.py
@@ -13,39 +13,31 @@ from osbuild.util.checksum import verify_file
 SOURCES_NAME = "org.osbuild.curl"
 
 TEST_SOURCE_PAIRS = [
-    (
+    {
         # sha256("0")
-        "sha256:5feceb66ffc86f38d952786c6d696c79c2dbc239dd4e91b46729d73a27fb57e9",
-        {
-            "url": "http://example.com/file/0",
-        },
-    ), (
+        "checksum": "sha256:5feceb66ffc86f38d952786c6d696c79c2dbc239dd4e91b46729d73a27fb57e9",
+        "url": "http://example.com/file/0",
+    }, {
         # sha256("1")
-        "sha256:6b86b273ff34fce19d6b804eff5a3f5747ada4eaa22f1d49c01e52ddb7875b4b",
-        {
-            "url": "http://example.com/file/1",
-            "insecure": True,
-        },
-    ), (
+        "checksum": "sha256:6b86b273ff34fce19d6b804eff5a3f5747ada4eaa22f1d49c01e52ddb7875b4b",
+        "url": "http://example.com/file/1",
+        "insecure": True,
+    }, {
         # sha256("2")
-        "sha256:d4735e3a265e16eee03f59718b9b5d03019c07d8b6c51f90da3a666eec13ab35",
-        {
-            "url": "http://example.com/file/2",
-            "secrets": {
-                "ssl_ca_cert": "some-ssl_ca_cert",
-            },
+        "checksum": "sha256:d4735e3a265e16eee03f59718b9b5d03019c07d8b6c51f90da3a666eec13ab35",
+        "url": "http://example.com/file/2",
+        "secrets": {
+            "ssl_ca_cert": "some-ssl_ca_cert",
         },
-    ), (
+    }, {
         # sha256("3")
-        "sha256:4e07408562bedb8b60ce05c1decfe3ad16b72230967de01f640b7e4729b49fce",
-        {
-            "url": "http://example.com/file/3",
-            "secrets": {
-                "ssl_client_cert": "some-ssl_client_cert",
-                "ssl_client_key": "some-ssl_client_key",
-            },
+        "checksum": "sha256:4e07408562bedb8b60ce05c1decfe3ad16b72230967de01f640b7e4729b49fce",
+        "url": "http://example.com/file/3",
+        "secrets": {
+            "ssl_client_cert": "some-ssl_client_cert",
+            "ssl_client_key": "some-ssl_client_key",
         },
-    ),
+    },
 ]
 
 
@@ -77,15 +69,15 @@ def test_curl_gen_download_config(tmp_path, sources_module):
     """)
 
 
-def test_split_chsum_desc_tuples(sources_module):
+def test_split_items(sources_module):
     # pylint: disable=W0212
-    split = sources_module._split_chksum_desc_tuples(TEST_SOURCE_PAIRS, 2)
+    split = sources_module._split_items(TEST_SOURCE_PAIRS, 2)
     assert len(split) == len(TEST_SOURCE_PAIRS) // 2
     for itms in split:
         assert len(itms) == 2
 
+    split = sources_module._split_items(TEST_SOURCE_PAIRS, 99)
     # pylint: disable=W0212
-    split = sources_module._split_chksum_desc_tuples(TEST_SOURCE_PAIRS, 99)
     assert len(split) == len(TEST_SOURCE_PAIRS)
     for itms in split:
         assert len(itms) == 1
@@ -112,7 +104,8 @@ def test_curl_download_many_fail(patched_run, tmp_path, sources_module):
 def test_curl_download_many(mocked_run, tmp_path, sources_module):
     def _fake_download(*args, **kwargs):
         download_dir = pathlib.Path(kwargs["cwd"])
-        for chksum, desc in TEST_SOURCE_PAIRS:
+        for desc in TEST_SOURCE_PAIRS:
+            chksum = desc["checksum"]
             (download_dir / chksum).write_text(desc["url"][-1], encoding="utf8")
         return subprocess.CompletedProcess(args, 0)
     mocked_run.side_effect = _fake_download
@@ -123,6 +116,7 @@ def test_curl_download_many(mocked_run, tmp_path, sources_module):
     curl.cache.mkdir(parents=True, exist_ok=True)
 
     curl.fetch_many(TEST_SOURCE_PAIRS)
-    for chksum, desc in TEST_SOURCE_PAIRS:
+    for desc in TEST_SOURCE_PAIRS:
+        chksum = desc["checksum"]
         assert (curl.cache / chksum).exists()
         assert verify_file(curl.cache / chksum, chksum)
