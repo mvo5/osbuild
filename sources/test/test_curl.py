8a373f9787d1800f4bf4fcda71358dc29da372a7
Michael Vogt <michael.vogt@gmail.com>
2024-02-06 09:45:36 +0100

    fixup! generate curl config

--- sources/test/test_curl.py
+++ sources/test/test_curl.py
@@ -3,12 +3,12 @@
 import pathlib
 import socket
 import subprocess
+import textwrap
 from unittest.mock import patch
 
 SOURCES_NAME = "org.osbuild.curl"
 
 hashes = {
-    "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855": b"",
     "5feceb66ffc86f38d952786c6d696c79c2dbc239dd4e91b46729d73a27fb57e9": b"0",
     "6b86b273ff34fce19d6b804eff5a3f5747ada4eaa22f1d49c01e52ddb7875b4b": b"1",
     "d4735e3a265e16eee03f59718b9b5d03019c07d8b6c51f90da3a666eec13ab35": b"2",
@@ -32,7 +32,7 @@ def fake_download(*args, **kwargs):
 
 
 @patch("subprocess.run")
-def test_curl_fetch_one(mocked_run, tmp_path, sources_module):
+def test_curl_fetch_all_integration(mocked_run, tmp_path, sources_module):
     mocked_run.side_effect = fake_download
     
     # source will automatically close the socket on __del__()
@@ -44,6 +44,57 @@ def test_curl_fetch_one(mocked_run, tmp_path, sources_module):
     desc = {
         "url": "http://example.com/file1",
     }
-    requested_file = "sha256:e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
+    requested_file = "sha256:6b86b273ff34fce19d6b804eff5a3f5747ada4eaa22f1d49c01e52ddb7875b4b"
     curl.fetch_all({requested_file: desc})
     assert (curl.cache / requested_file).exists()
+
+
+TEST_SOURCE_ITEMS = {
+    "sha256:5feceb66ffc86f38d952786c6d696c79c2dbc239dd4e91b46729d73a27fb57e9": {
+        "url": "http://example.com/file0",
+    },
+    "sha256:6b86b273ff34fce19d6b804eff5a3f5747ada4eaa22f1d49c01e52ddb7875b4b": {
+        "url": "http://example.com/file1",
+        "insecure": True,
+    },
+    "sha256:d4735e3a265e16eee03f59718b9b5d03019c07d8b6c51f90da3a666eec13ab35": {
+        "url": "http://example.com/file2",
+        "secrets": {
+            "ssl_ca_cert": "some-ssl_ca_cert",
+        },
+    },
+    "sha256:4e07408562bedb8b60ce05c1decfe3ad16b72230967de01f640b7e4729b49fce": {
+        "url": "http://example.com/file3",
+        "secrets": {
+            "ssl_client_cert": "some-ssl_client_cert",
+            "ssl_client_key": "some-ssl_client_key",
+        },
+    },
+}
+
+
+def test_curl_gen_download_config(tmp_path, sources_module):
+    dst_path = tmp_path / "curl-config.txt"
+    sources_module._gen_curl_download_config(TEST_SOURCE_ITEMS, dst_path)
+    assert dst_path.exists()
+    assert dst_path.read_text(encoding="utf8") == textwrap.dedent(f"""\
+    url = "http://example.com/file0"
+    output = "sha256:5feceb66ffc86f38d952786c6d696c79c2dbc239dd4e91b46729d73a27fb57e9"
+    no-insecure
+
+    url = "http://example.com/file1"
+    output = "sha256:6b86b273ff34fce19d6b804eff5a3f5747ada4eaa22f1d49c01e52ddb7875b4b"
+    insecure
+
+    url = "http://example.com/file2"
+    output = "sha256:d4735e3a265e16eee03f59718b9b5d03019c07d8b6c51f90da3a666eec13ab35"
+    cacert = "some-ssl_ca_cert"
+    no-insecure
+
+    url = "http://example.com/file3"
+    output = "sha256:4e07408562bedb8b60ce05c1decfe3ad16b72230967de01f640b7e4729b49fce"
+    cert = "some-ssl_client_cert"
+    key = "some-ssl_client_key"
+    no-insecure
+
+    """)
